290e85d776356833485d6491bc5d1f58e8e331f7
Olakotan Daniel <officialhorlakz@gmail.com>
2024-09-29 22:31:36 +0100

    refactor(auth module): sort imports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
